a44316d4dd6487501222b23885f56eb91841d778
mmerabet <mhmd.merabet@gmail.com>
2026-04-25 12:50:53 +0200

    feat:(auth) SHOP-4 - forgot password tests placeholder - Sprint 1

---
+++ tests/auth/forget-password.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('SHOP-4 - Réinitialisation du mot de passe', () => {
+
+    test.fixme('AC1 - Email de réinitialisation envoyé', async ({ page }) => {
+        // Feature non disponible sur automationexercise.com
+        // Kevin n'a pas encore livré le backend SendGrid
+        // À implémenter quand l'environnement ShopEase sera disponible
+    });
+
+    test.fixme('AC2 - Lien expire après 1 heure', async ({ page }) => {
+        // Feature non disponible sur automationexercise.com
+    });
+
+    test.fixme('AC3 - Lien valide redirige vers page réinitialisation', async ({ page }) => {
+        // Feature non disponible sur automationexercise.com
+    });
+
+    test.fixme('AC4 - Lien expiré ou déjà utilisé redirige vers page erreur', async ({ page }) => {
+        // Feature non disponible sur automationexercise.com
+    });
+
+    test.fixme('AC5 - Règles de complexité mot de passe respectées', async ({ page }) => {
+        // Feature non disponible sur automationexercise.com
+    });
+
+    test.fixme('AC6 - Double saisie mot de passe avec confirmation', async ({ page }) => {
+        // Feature non disponible sur automationexercise.com
+    });
+
+    test.fixme('AC7 - Redirection vers page connexion après succès', async ({ page }) => {
+        // Feature non disponible sur automationexercise.com
+    });
+
+});
