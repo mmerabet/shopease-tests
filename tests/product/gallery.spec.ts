c7c8b8336a5974026f3e759f963de41a0c0a547f
mmerabet <mhmd.merabet@gmail.com>
2026-04-27 08:19:45 +0200

    feat(product): SHOP-6/7/8 - product tests skeleton - Sprint 1

---
+++ tests/product/gallery.spec.ts
@@ -0,0 +1,29 @@
+import { test } from '@playwright/test';
+
+test.describe('SHOP-7 - Galerie photo', () => {
+
+    test.fixme('AC1 - Au moins une photo affichée', async ({ page }) => {
+        // À implémenter sur environnement ShopEase - Sprint 2
+    });
+
+    test.fixme('AC2 - Placeholder si photos indisponibles', async ({ page }) => {
+        // À implémenter sur environnement ShopEase - Sprint 2
+    });
+
+    test.fixme('AC3 - Première photo en grand, miniatures en dessous', async ({ page }) => {
+        // À implémenter sur environnement ShopEase - Sprint 2
+    });
+
+    test.fixme('AC4 - Clic pour zoomer', async ({ page }) => {
+        // À implémenter sur environnement ShopEase - Sprint 2
+    });
+
+    test.fixme('AC5 - Navigation flèches et miniatures', async ({ page }) => {
+        // À implémenter sur environnement ShopEase - Sprint 2
+    });
+
+    test.fixme('AC6 - Photos changent selon couleur sélectionnée', async ({ page }) => {
+        // À implémenter sur environnement ShopEase - Sprint 2
+    });
+
+});
